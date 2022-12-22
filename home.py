c824a9447104072b1f46a5d3cf8c7988fe1c566d
Conorjurewicz <jurewiczconor7@gmail.com>
2022-12-22 12:22:00 -0500

    Added routes for new directories

--- home.py
+++ home.py
@@ -2,7 +2,19 @@ from flask import Blueprint, render_template, request, jsonify, redirect, url_fo
 
 home = Blueprint(__name__, "home")
 
+
 @home.route("/")
 def home_dir():
     return render_template("index.html")
     
+@home.route("/mustreads")
+def mustreads():
+    return render_template("mustreads.html")
+    
+@home.route("/mybooks")
+def mybooks():
+    return render_template("mybooks.html")
+
+@home.route("/bookreviews")
+def bookreviews():
+    return render_template("bookreviews.html")
